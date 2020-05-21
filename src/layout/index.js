71f1fed7b2b495e322f4d0bf32c3b8f327137766
Lucas Medina <lucasmedina99.lm@gmail.com>
2020-05-21 11:47:21 -0400

    gregar el homepage de los usuarios y el layout para todas las paginas internas del proyecto

---
+++ src/layout/index.js
@@ -0,0 +1,32 @@
+const yo = require('yo-yo');    
+
+module.exports = function layout(content){
+    return yo`
+<div>
+<nav class="header">
+<div class="nav-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6 offset-m1">
+                <a href="/" class="platzigram brand-logo">Platzigram</a>
+            </div>
+            <div class="col s2 m6 push-m10 push-s10">
+                <a href="#" class="btn btn-large btn-flat dropdown-button" data-activate="drop-user">
+                    <i class="fas fa-user"></i>
+                </a>
+                <ul class="dropdown-content" id="drop-user">
+                    <li><a href="#">Salir</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</nav>
+<div class="content">
+    ${content}
+</div>
+</div>
+`
+}
+
+    
